849b20fb266a4deefcab0fc36d71a692610347fd
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-12-20 15:29:23 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <p>Hi</p>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    }
+};
+
+module.exports = config;
