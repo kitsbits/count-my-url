4ac48b56b53467860e86e2ac428bf72b6c4371cf
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-12-23 23:53:47 -0500

    update file structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,12 @@ const path = require("path");
 const config = {
     entry: "./index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "build/js/"),
+        publicPath: "/public/assets/js/",
         filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: "public"
     },
     module: {
         rules: [
